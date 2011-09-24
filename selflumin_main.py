9c8b2f3a04fe08d30ead8edc93a6fed462b22705
unknown <nhn@.(none)>
2011-09-24 11:27:25 +0900

    Add index, photo

--- selflumin_main.py
+++ selflumin_main.py
@@ -2,21 +2,42 @@
 
 import cgi
 import xml.dom.minidom
-import selumin
 import zipfile
 import os
+import selumin
 
 PostData = cgi.FieldStorage()
 PlatformType = PostData['platformtype'].value
 UserID = PostData['getid'].value
 TwitList = []
+
 if PlatformType == 'me2':
   TwitList = selumin.user_timeline_me2(UserID)
-elif PlatformType == 'twitter'
-  TwitList = selumin.user_timeline(UserID)
+elif PlatformType == 'twitter':
+  TwitList = selumin.user_timeline(UserID)  
+  
 newdoc = xml.dom.minidom.Document()  
 TopNode = newdoc.createElement('twits')
 newdoc.appendChild(TopNode)
+
+def IndexSplit(List, n):
+  IndexLength = len(List) - 1
+  Interval = ((IndexLength) / n)
+  retList = []
+  for i in range(n * 2):
+    Index = i * Interval
+    IndexNode = {}
+    if Index >= IndexLength or (IndexLength - Index) < (Interval / 2):
+      Twit = List[IndexLength]      
+      IndexNode[str(IndexLength)] = str(Twit['date'])
+      retList.append(IndexNode)
+      break
+    Twit = List[Index]      
+    IndexNode[str(Index)] = str(Twit['date'])
+    retList.append(IndexNode)
+  return retList
+
+IndexList = IndexSplit(TwitList, 4)
 
 print 'Content-Type text/html\n\n'
 print '<html>'
@@ -24,40 +45,59 @@ print '<head>'
 print '<title>selflumin main</title>'
 print '<style type="text/css">'
 print '.box {width: 500px;padding: 10px;}'
-print '.b1f, .b2f, .b3f, .b4f{height:1px;font-size:1px;overflow:hidden;display:block;background:#ddd;}' 
+print '.b1f, .b2f, .b3f, .b4f{height:1px;font-size:1px;overflow:hidden;display:block;background:#B3C5F3;}' 
 print '.b1f {margin:0 5px;}' 
 print '.b2f {margin:0 3px;}' 
 print '.b3f {margin:0 2px;}' 
 print '.b4f {margin:0 1px;}' 
-print '.content {padding: 10px;background: #ddd;}' 
+print '.content {padding: 10px;background: #B3C5F3;}' 
 print '</style>'
+print '<script language="JavaScript">'
+print '</script>'
 print '</head>'
 print '<body>'
-print '<a href=./Download/' + UserID + '.zip>파일 다운로드</a>'
+print '<a href=./Download/' + UserID + '.zip>파일 다운로드</a><br>'
+for IndexNode in IndexList:
+  for Index, Date in IndexNode.iteritems():
+    print '<a href=#' + Index + '>[' + ' ' + Date + ']</a>'
 RTCount = 0
 Me2Count = 0
+PhotoUrl = None
+Index = 0;
+
+print '<table>'
+print '<tr>'
+print '<td>'
 for twit in TwitList:
   TwitDate = str(twit['date'])
-  TwitText = twit['text'].encode('cp949')
+  try:
+    TwitText = twit['text'].encode('cp949')
+  except:
+    Index += 1
+    continue
   if PlatformType == 'me2':
     Me2Count = twit['me2_count']
-  elif PlatformType == 'twitter'
+  elif PlatformType == 'twitter':
     RTCount = str(twit['rt_count'])
   print '<div class="box">'
   print '    <b class="b1f"></b>'
   print '    <b class="b2f"></b>'
   print '    <b class="b3f"></b>'
   print '    <b class="b4f"></b>'
-  print '    <div class="content">'
+  print '    <div class="content"><a name=' + str(Index) + '>'
   print TwitDate
   print '<br>'
+  if PlatformType == 'me2':
+    if not twit['photo_url'] == '':
+      PhotoUrl = twit['photo_url']
+      print '<a href="' + PhotoUrl + '?type=w500"  target="_blank"><img src="' + PhotoUrl + '?type=s44"></a>'
   print TwitText
   print '<br>'  
   if PlatformType == 'me2':
     print 'Me2 Count : ' + Me2Count
-  elif PlatformType == 'twitter'
+  elif PlatformType == 'twitter':
     print 'RT Count : ' + RTCount
-  print '    </div>'
+  print '    </a><br><a href=#top>[Top]</a></div>'
   print '    <b class="b4f"></b>'
   print '    <b class="b3f"></b>'
   print '    <b class="b2f"></b>'
@@ -81,10 +121,20 @@ for twit in TwitList:
   CountText = 0
   if PlatformType == 'me2':
     CountText = Me2Count
-  elif PlatformType == 'twitter'
+  elif PlatformType == 'twitter':
     CountText = RTCount
   NewCount = newdoc.createTextNode(CountText)
   CountNode.appendChild(NewCount)
+  
+  if PlatformType == 'me2':
+    PhotoNode = newdoc.createElement('photourl')
+    TwitNode.appendChild(PhotoNode)
+    NewUrl = newdoc.createTextNode(PhotoUrl)
+    PhotoNode.appendChild(NewUrl)
+  Index += 1
+print '</td>'
+print '</tr>'
+print '</table>'
 print '</body>'
 print '</html>'
 
